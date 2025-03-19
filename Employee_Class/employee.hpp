ca6dda50f30741435d618d07ac696bf26b159862
Abel T mac <tshiminiabel6@gmail.com>
2025-03-18 22:04:23 -0400

    change the name of the accessors functions ot have snake case convention

--- Employee_Class/employee.hpp
+++ Employee_Class/employee.hpp
@@ -13,10 +13,14 @@
         Employee( std::string newName , int newId , std::string newDepartement , std::string newPosition) {} ; 
 
         //name setter and getter 
-        std::string get_name() {} ; 
-        std::string set_name(std::string name) {} ; 
+        std::string getName() {} ; 
+        std::string setName(std::string name) {} ; 
 
-        //
+        //id number setter and getter 
+        int get_id() {} ; 
+        int set_name(int newId) {} ; 
+
+        //id 
 
 
 
